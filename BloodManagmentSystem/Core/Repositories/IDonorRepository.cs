6aaa1a0523e842fec4eb2b24e8c4f75419ab95e3
Marta Sajdak <crappylime@gmail.com>
2017-10-04 10:23:45 +0200

    Implement add method for Donor in DonorRepository and its interface

---
+++ BloodManagmentSystem/Core/Repositories/IDonorRepository.cs
@@ -0,0 +1,9 @@
+﻿using BloodManagmentSystem.Core.Models;
+
+namespace BloodManagmentSystem.Core.Repositories
+{
+    public interface IDonorRepository
+    {
+        void Add(Donor donor);
+    }
+}
