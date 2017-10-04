42c5140347effbde2859574fbd64efddad2664a8
Cezary Broś <cezary.bros+git@gmail.com>
2017-10-04 11:56:12 +0200

    Fix Detail blood type display

---
+++ BloodManagmentSystem/Persistance/Migrations/201710040847514_RenameDateColumn.cs
@@ -0,0 +1,20 @@
+namespace BloodManagmentSystem.Persistance.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class RenameDateColumn : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.BloodRequests", "DueDateTime", c => c.DateTime(nullable: false));
+            DropColumn("dbo.BloodRequests", "DueDate");
+        }
+        
+        public override void Down()
+        {
+            AddColumn("dbo.BloodRequests", "DueDate", c => c.DateTime(nullable: false));
+            DropColumn("dbo.BloodRequests", "DueDateTime");
+        }
+    }
+}
