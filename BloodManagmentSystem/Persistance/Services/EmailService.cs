c1454d209b4a8b50c897fda4932300fed473b9dc
Cezary Broś <cezary.bros+git@gmail.com>
2017-10-04 12:30:29 +0200

    Add email service, Add email view

---
+++ BloodManagmentSystem/Persistance/Services/EmailService.cs
@@ -0,0 +1,26 @@
+﻿using BloodManagmentSystem.Core.Models;
+using Postal;
+using System.Collections.Generic;
+
+namespace BloodManagmentSystem.Persistance.Services
+{
+    public class EmailService
+    {
+        public ICollection<Donor> Recipients { get; private set; }
+
+        public EmailService(ICollection<Donor> donors)
+        {
+            Recipients = donors;
+        }
+
+        public void SendEmails()
+        {
+            foreach (var recipient in Recipients)
+            {
+                dynamic email = new Email("Confirmation");
+                email.To = recipient.Email;
+                email.SendAsync();
+            }
+        }
+    }
+}
