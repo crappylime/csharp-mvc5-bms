925ee6857c029c1faf351cbbd5ad6f8e40558ea3
Cezary Broś <cezary.bros+git@gmail.com>
2017-10-07 18:04:29 +0200

    Add confirmation table

---
+++ BloodManagmentSystem/Core/Models/Confirmation.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BloodManagmentSystem.Core.Models
+{
+    public class Confirmation
+    {
+        [Key]
+        [Column(Order = 1)]
+        public int RequestId { get; set; }
+
+        [Key]
+        [Column(Order = 2)]
+        public int DonorId { get; set; }
+
+        public BloodRequest Request { get; set; }
+        public Donor Donor { get; set; }
+        public string HashCode { get; set; }
+        public bool Status { get; set; }
+    }
+}
---
+++ BloodManagmentSystem/Persistance/Repositories/ConfirmationRepository.cs
@@ -0,0 +1,16 @@
+﻿using BloodManagmentSystem.Core.Models;
+
+namespace BloodManagmentSystem.Persistance.Repositories
+{
+    public class ConfirmationRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ConfirmationRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+
+    }
+}
