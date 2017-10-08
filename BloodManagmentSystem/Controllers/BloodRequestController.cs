bf4f2e8b8b42d76cdee891ab6fb001b185c38181
Cezary Broś <cezary.bros+git@gmail.com>
2017-10-08 21:38:06 +0200

    Implement creating confirmations

--- BloodManagmentSystem/Controllers/BloodRequestController.cs
+++ BloodManagmentSystem/Controllers/BloodRequestController.cs
@@ -2,6 +2,9 @@
 using BloodManagmentSystem.Core.Models;
 using BloodManagmentSystem.Core.ViewModels;
 using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
 using System.Threading.Tasks;
 using System.Web.Mvc;
 
@@ -75,9 +78,43 @@ namespace BloodManagmentSystem.Controllers
             if (!(TempData["Donors"] is IEnumerable<Donor> donors))
                 return HttpNotFound();
 
+            var confirmations = CreateConfirmations(donors);
+
+            _unitOfWork.Confirmations.AddRange(confirmations);
+            _unitOfWork.Complete();
+
             Task.Factory.StartNew(() => SendEmails(donors));
 
             return RedirectToAction("Index");
+        }
+
+        #region PrivateMethods
+
+        private IEnumerable<Confirmation> CreateConfirmations(IEnumerable<Donor> donors)
+        {
+            return donors.Select(donor => new Confirmation
+            {
+                DonorId = donor.Id,
+                RequestId = 1,
+                HashCode = GetMd5HashCode($"{donor.Id}dnr"),
+                Status = false
+            }).ToList();
+        }
+
+        private string GetMd5HashCode(string input)
+        {
+            var md5 = MD5.Create();
+
+            var result = md5.ComputeHash(Encoding.UTF8.GetBytes(input));
+
+            var sb = new StringBuilder();
+
+            foreach (var b in result)
+            {
+                sb.Append(b.ToString("x2"));
+            }
+
+            return sb.ToString();
         }
 
         private void SendEmails(IEnumerable<Donor> donors)
@@ -88,5 +125,7 @@ namespace BloodManagmentSystem.Controllers
                 email.SendAsync();
             }
         }
+
+        #endregion
     }
 }
--- BloodManagmentSystem/Core/Models/Confirmation.cs
+++ BloodManagmentSystem/Core/Models/Confirmation.cs
@@ -13,9 +13,13 @@ namespace BloodManagmentSystem.Core.Models
         [Column(Order = 2)]
         public int DonorId { get; set; }
 
+        [Required]
+        public string HashCode { get; set; }
+
+        [Required]
+        public bool Status { get; set; }
+
         public BloodRequest Request { get; set; }
         public Donor Donor { get; set; }
-        public string HashCode { get; set; }
-        public bool Status { get; set; }
     }
 }
